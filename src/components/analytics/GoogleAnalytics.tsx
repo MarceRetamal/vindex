8752265332e618efe00a1c705526e92fc2e8f212
MARCELO RETAMAL <86079681+MarceRetamal@users.noreply.github.com>
2026-07-22 18:17:20 -0300

    ga4

---
+++ src/components/analytics/GoogleAnalytics.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import Script from 'next/script'
+
+const GA_MEASUREMENT_ID = 'G-V20S3PF7YK'
+
+export function GoogleAnalytics() {
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="ga4-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          window.gtag = gtag;
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+      </Script>
+    </>
+  )
+}
